e9fc34113476446bf1da9bf4c52ac88ec2341dbc
Gregory-Hub <novikov.gri@gmail.com>
2025-11-01 15:35:07 +0300

    Create entity base classes

---
+++ Game011125/Source/Game011125/Public/APlayerCharacter.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "APlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class GAME011125_API UAPlayerCharacter : public UObject
+{
+	GENERATED_BODY()
+};
