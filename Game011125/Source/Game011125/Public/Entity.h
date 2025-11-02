6e051abf60a00349821d79a80c1bfadf55805eee
Maxim Devkin <makintosh365@gmail.com>
2025-11-03 02:29:57 +0300

    feat(entity): implement base entity class

--- Game011125/Source/Game011125/Public/Entity.h
+++ Game011125/Source/Game011125/Public/Entity.h
@@ -1,17 +1,50 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Character.h"
+#include "GameObject.h"
+
+#include "Interfaces/ICanUseAbilities.h"
+#include "Interfaces/ICanTakeDamage.h"
+#include "Interfaces/ICanUseAbilities.h"
+#include "Interfaces/ICanUseBuffs.h"
+
 #include "Entity.generated.h"
 
-UCLASS(Abstract, Blueprintable)
-class GAME011125_API AEntity : public ACharacter
+class UAbility;
+class UBuff;
+
+UCLASS(Abstract)
+class GAME011125_API AEntity 
+	: public AGameObject
+	, public ICanTakeDamage
+	, public ICanUseAbilities
+	, public ICanUseBuffs
 {
+
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AEntity();
+
+	void Tick(float DeltaTime) override;
+
+	// ICanUseAbilities
+	bool UseCurrentAbility() override;
+
+	// ICanTakeDamage
+	bool TakeDamage(float damage) override;
+
+	// ICanUseBuffs
+	bool UseBuff() override;
+
+protected:
+	//--------------------------------------------------------------------------
+	// --- Abilities
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Entity|Abilities")
+	TArray<TObjectPtr<UAbility>> Abilities;
+
+	//--------------------------------------------------------------------------
+	// --- Buffs
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Entity|Buffs")
+	TArray<TObjectPtr<UBuff>> ActiveBuffs;
 };
