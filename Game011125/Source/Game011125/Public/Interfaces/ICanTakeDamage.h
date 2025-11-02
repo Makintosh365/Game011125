2bd6a1d1b393db20d30a8f414fc2e8ad4649b36b
Gregory-Hub <novikov.gri@gmail.com>
2025-11-02 14:34:50 +0300

    Create interfaces

---
+++ Game011125/Source/Game011125/Public/Interfaces/ICanTakeDamage.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ICanTakeDamage.generated.h"
+
+UINTERFACE(MinimalAPI, Blueprintable)
+class UCanTakeDamage : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class ICanTakeDamage
+{
+	GENERATED_BODY()
+
+public:
+	virtual void TakeDamage(float damage);
+};
