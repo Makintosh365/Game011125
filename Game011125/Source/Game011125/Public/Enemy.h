b7f0a535ab0f7c62bf8d377e88aa06eb9d9adf4e
Makintosh365 <156786742+Makintosh365@users.noreply.github.com>
2025-11-03 02:22:49 +0300

    Refactor comments and formatting in Enemy.h

--- Game011125/Source/Game011125/Public/Enemy.h
+++ Game011125/Source/Game011125/Public/Enemy.h
@@ -14,31 +14,21 @@ class GAME011125_API AEnemy : public AEntity
 	GENERATED_BODY()
 
 public:
-	//--------------------------------------------------------------------------
-	// --- Конструктор
+
 	AEnemy();
-	//--------------------------------------------------------------------------
-	// --- Тик
+
 	virtual void Tick(float DeltaTime) override;
 
-	//--------------------------------------------------------------------------
-	// --- Цель (лидер, не игрок)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "References")
 	AHero* HeroTarget = nullptr;
 
-	//--------------------------------------------------------------------------
-	// --- Задать цель из блюпринта/кода
 	UFUNCTION(BlueprintCallable, Category = "References")
 	void SetHeroTarget(AHero* NewHero);
 
-	//--------------------------------------------------------------------------
-	// --- Параметры движения (вот тут самое важное — тут есть FollowDistance!)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
-	float FollowDistance = 150.0f;   // на каком расстоянии останавливаемся
+	float FollowDistance = 150.0f;   
 
 protected:
-	//--------------------------------------------------------------------------
-	// --- Жизненный цикл
 	virtual void BeginPlay() override;
 
 
