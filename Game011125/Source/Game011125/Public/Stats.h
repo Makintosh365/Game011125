05ea3cdd245ef03abcad5bb8b93c99abb0a69078
Maxim Devkin <makintosh365@gmail.com>
2025-11-03 01:55:39 +0300

    feat(stats): add XP fields and make base stats editable in BP

--- Game011125/Source/Game011125/Public/Stats.h
+++ Game011125/Source/Game011125/Public/Stats.h
@@ -15,18 +15,19 @@ public:
     // --- Base values (defined by unit type / class)
     //--------------------------------------------------------------------------
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Stats")
     float BaseMaxHP = 100.f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Stats")
     float BaseDamage = 10.f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Stats")
     float BaseSpeed = 400.f;
 
     //--------------------------------------------------------------------------
     // --- Current values (actual values used in combat)
     //--------------------------------------------------------------------------
+
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
     float CurrentMaxHP = 100.f;
 
@@ -38,6 +39,22 @@ public:
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats")
     float CurrentSpeed = 400.f;
+
+
+    //----------------------------------------------------------------------
+    // --- Progression / XP
+    //----------------------------------------------------------------------
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats|XP")
+    int32 TotalXP = 0;
+
+    // текущий уровень юнита
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats|XP")
+    int32 Level = 1;
+
+    // сколько опыта даст убийство Ё“ќ√ќ юнита
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Stats|XP")
+    int32 XPRewardOnDeath = 10;
 
     // Restore HP to current max value
     void ResetHP()
@@ -57,8 +74,15 @@ static inline FStats MakePlayerDefaultStats()
     S.CurrentDamage = 15.f;
     S.BaseSpeed = 500.f;
     S.CurrentSpeed = 500.f;
+
+ 
+    S.TotalXP = 0;
+    S.Level = 1;
+    S.XPRewardOnDeath = 0; 
+
     return S;
 }
+
 // Default enemy stats
 static inline FStats MakeEnemyDefaultStats()
 {
@@ -70,5 +94,11 @@ static inline FStats MakeEnemyDefaultStats()
     S.CurrentDamage = 7.f;
     S.BaseSpeed = 250.f;
     S.CurrentSpeed = 250.f;
+
+    S.TotalXP = 0;       
+    S.Level = 1;
+    S.XPRewardOnDeath = 15; 
+
     return S;
 }
+s
